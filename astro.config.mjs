be3d26d2d1d04ef37ddd929c69937695462e837e
Joe Duncko <JoeDuncko@users.noreply.github.com>
2024-08-14 09:59:38 -0400

    Let's try deploying to GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://swbf2.events",
     integrations: [
         AstroPWA({
             workbox: {
